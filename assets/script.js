34547aff63ee678245248448bfa4258191391280
Manuel Francisco <ciesafrancisco@hotmail.com>
2023-06-20 21:00:58 +0200

    Update new comments in js

--- assets/script.js
+++ assets/script.js
@@ -18,26 +18,28 @@
    },
  ];
 
+
+ // verification of the paths ok
+
 /* for (const slide of slides) {
   console.log(slide.image);
 } */
  
 
-
-
-// Variables
+// Variables connecting to Html
 
 const bannerImg = document.querySelector('.banner-img');
 const tagLine = document.querySelector('#banner p');
 const dotsContainer = document.querySelector('.dots');
 
 
+ // verification if there is a contact 
+
 /* console.log(bannerImg)
 console.log(tagLine)
 console.log(dotsContainer) */
 
-
-
+// for keep trac of the current index of the display image   
 let currentSlideIndex = 0;
 
 
@@ -58,7 +60,7 @@ function updateSlide() {
 }
 
 
-
+// function to be called upon when the click event on the arrow prev happens
 function goToPreviousSlide() {
 	// console.log('I was clicked, I am previous!'); 
   currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
@@ -66,6 +68,8 @@ function goToPreviousSlide() {
   updateSlide();
 }
 
+
+// function to be called upon when the click event on the arrow next happens
 function goToNextSlide() {
 	//   console.log('I was clicked, I am next!');
   currentSlideIndex = (currentSlideIndex + 1) % slides.length;
@@ -75,24 +79,27 @@ function goToNextSlide() {
 
 
 
-// Add event listeners to the arrow buttons
+// creating the arrows variables
 const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
 
 
-
+// Add event listeners to the arrow buttons with a click event, 
 arrowLeft.addEventListener('click', goToPreviousSlide);
 
 arrowRight.addEventListener('click', goToNextSlide);
 
 
 slides.forEach(function (slide, index) {
+  //creation of the div with class of "dot" which is already in the css rules
   const dot = document.createElement('div');
   dot.classList.add('dot');
+  // Add event listeners to the dots
   dot.addEventListener('click', function () {
     currentSlideIndex = index;
     updateSlide();
   });
+  // append the div to the parent div which is already in the html
   dotsContainer.appendChild(dot);
 });
 
